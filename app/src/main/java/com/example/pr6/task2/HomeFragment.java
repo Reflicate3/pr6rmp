e5764cab9f959897b735b59ebd2482576c695e42
reflicate3 <reflicate3@gmail.com>
2024-05-27 01:14:22 +0300

    pr6 done

---
+++ app/src/main/java/com/example/pr6/task2/HomeFragment.java
@@ -0,0 +1,26 @@
+package com.example.pr6.task2;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.fragment.app.Fragment;
+
+import com.example.pr6.R;
+
+
+public class HomeFragment extends Fragment {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState)
+    {
+        return inflater.inflate(R.layout.fragment_home, container, false);
+    }
+}
